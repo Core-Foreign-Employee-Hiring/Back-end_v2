c7125e11282b1d03995ee2d8e4416a0449dbd601
22DT <qab3312@gmail.com>
2025-06-16 11:25:25 +0900

    [FIX] 채용 후기 관련 api 오타 수정

--- src/main/java/com/forwork/backend/api/recruit_review/controller/RecruitReviewController.java
+++ src/main/java/com/forwork/backend/api/recruit_review/controller/RecruitReviewController.java
@@ -50,7 +50,7 @@ public class RecruitReviewController {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "후기 등록 성공"),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "해당 사용자를 찾을 수 없습니다."),
     })
-    @PostMapping("/")
+    @PostMapping
     public ResponseEntity<ApiResponse<Void>> save(@AuthenticationPrincipal SecurityMember securityMember,
                                                   @RequestBody RecruitReviewCreateDTO dto) {
         recruitReviewService.createRecruitReview(securityMember.getId(), dto);
@@ -89,7 +89,7 @@ public class RecruitReviewController {
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "후기 조회 성공"),
     })
-    @GetMapping("/")
+    @GetMapping
     public ResponseEntity<ApiResponse<PageResponseDTO<RecruitReviewPreviewResponseDTO>>> getRecruits(
             @Parameter(description = "검색 키워드", in = ParameterIn.QUERY)
             @RequestParam(value = "keyword", required = false) String keyword,
