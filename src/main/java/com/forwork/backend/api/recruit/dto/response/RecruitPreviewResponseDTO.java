018ff7e68b7ca5662db86919a136557d8d6c6ecc
22DT <qab3312@gmail.com>
2026-02-07 21:37:13 +0900

    [FEAT] 공고 전체 조회 수정

--- src/main/java/com/forwork/backend/api/recruit/dto/response/RecruitPreviewResponseDTO.java
+++ src/main/java/com/forwork/backend/api/recruit/dto/response/RecruitPreviewResponseDTO.java
@@ -1,10 +1,10 @@
 package com.forwork.backend.api.recruit.dto.response;
 
-import com.forwork.backend.api.member.entity.JobCategory;
-import com.forwork.backend.api.recruit.entity.Recruit;
-import com.forwork.backend.api.recruit.entity.RecruitJobCategory;
-import com.forwork.backend.api.recruit.enums.ContractType;
-import com.forwork.backend.api.recruit.enums.SalaryType;
+import com.forwork.backend.api.member.entity.JobRole;
+import com.forwork.backend.api.member.entity.Visa;
+import com.forwork.backend.api.recruit.entity.*;
+import com.forwork.backend.api.recruit.enums.CarrerType;
+import com.forwork.backend.api.recruit.enums.LanguageType;
 import com.forwork.backend.api.recruit.service.RecruitUtils;
 import io.swagger.v3.oas.annotations.media.Schema;
 
@@ -15,48 +15,61 @@ import java.util.Set;
 public record RecruitPreviewResponseDTO(
         @Schema(description = "공고 id")
         Long recruitId,
-        @Schema(description = "회사 사진")
-        String companyImageUrl,
-        @Schema(description = "회사명")
-        String companyName,
-        @Schema(description = "모집 종료 기간")
-        LocalDate recruitEndDate,
+
         @Schema(description = "공고명")
         String title,
+
         @Schema(description = "직무")
-        List<JobCategory> jobCategories,
-        @Schema(description = "급여 종류")
-        SalaryType salaryType,
-        @Schema(description = "급여")
-        Integer salary,
-        @Schema(description = "계약 형태 (ENUM: 정규직, 계약직, 프리랜서 등)")
-        ContractType contractType,
-        @Schema(description = "우편번호")
-        String zipcode,
-        @Schema(description = "주소")
-        String address1,
-        @Schema(description = "상세주소")
-        String address2
+        List<JobRole> jobRoles,
+
+        @Schema(description = "경력 형태 (ENUM: 신입, 경력)")
+        CarrerType carrerType,
+
+        @Schema(description = "경력 형태 직접 입력 (기타 선택 시 값 입력)")
+        String directInputCarrerType,
+
+        @Schema(description = "비자")
+        List<Visa> visas,
+
+        @Schema(description = "언어")
+        List<LanguageType> languageTypes,
+
+        @Schema(description = "근무지 주소")
+        String workAddress1,
+
+        @Schema(description = "모집 종료 기간")
+        LocalDate recruitEndDate,
+
+        @Schema(description = "회사 사진")
+        String companyImageUrl,
+
+        @Schema(description = "회사명")
+        String companyName
 ) {
 
     public static RecruitPreviewResponseDTO fromEntity(Recruit recruit) {
 
-        Set<RecruitJobCategory> recruitJobCategories = recruit.getRecruitJobCategories();
-        List<JobCategory> jobCategories = RecruitUtils.convertToJobCategories(recruitJobCategories);
+        Set<RecruitJobRole> recruitJobRoles = recruit.getRecruitJobRoles();
+        List<JobRole> jobRoles = JobRole.convertToJobRolesByRecruit(recruitJobRoles);
+
+        Set<RecruitVisa> recruitVisas = recruit.getRecruitVisas();
+        List<Visa> visas = RecruitUtils.convertToVisas(recruitVisas);
+
+        Set<RecruitLanguageType> recruitLanguageTypes = recruit.getRecruitLanguageTypes();
+        List<LanguageType> languageTypes = RecruitUtils.convertToLanguageTypes(recruitLanguageTypes);
 
         return new RecruitPreviewResponseDTO(
                 recruit.getId(),
-                recruit.getCompanyImageUrl(),
-                recruit.getCompanyName(),
-                recruit.getRecruitEndDate(),
                 recruit.getTitle(),
-                jobCategories,
-                recruit.getSalaryType(),
-                recruit.getSalary(),
-                recruit.getContractType(),
-                recruit.getCompanyZipcode(),
-                recruit.getCompanyAddress1(),
-                recruit.getCompanyAddress2()
+                jobRoles,
+                recruit.getCarrerType(),
+                recruit.getDirectInputCarrerType(),
+                visas,
+                languageTypes,
+                recruit.getWorkAddress1(),
+                recruit.getRecruitEndDate(),
+                recruit.getCompanyImageUrl(),
+                recruit.getCompanyName()
         );
     }
 }
