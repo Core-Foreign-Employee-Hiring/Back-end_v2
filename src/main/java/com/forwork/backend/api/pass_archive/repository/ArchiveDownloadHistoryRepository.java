49d95cd4180a68946266097728fdb17c8aa638e1
22DT <qab3312@gmail.com>
2026-03-21 15:04:58 +0900

    [FEAT] 다운로드한 아카이브 id 조회 쿼리 추가

--- src/main/java/com/forwork/backend/api/pass_archive/repository/ArchiveDownloadHistoryRepository.java
+++ src/main/java/com/forwork/backend/api/pass_archive/repository/ArchiveDownloadHistoryRepository.java
@@ -2,6 +2,14 @@ package com.forwork.backend.api.pass_archive.repository;
 
 import com.forwork.backend.api.pass_archive.entity.ArchiveDownloadHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ArchiveDownloadHistoryRepository extends JpaRepository<ArchiveDownloadHistory, Long> {
+
+    @Query("select distinct h.passArchive.passArchiveId from ArchiveDownloadHistory h" +
+            " where h.buyer.id=:buyerId")
+    List<Long> findPassArchiveIdsByBuyerId(@Param("buyerId") Long buyerId);
 }
