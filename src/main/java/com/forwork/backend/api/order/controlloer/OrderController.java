8fbafa7d5063e2804d50113f5f242d7ebd8caf42
22DT <qab3312@gmail.com>
2026-03-24 16:28:27 +0900

    [FEAT] ApiResponse success 오버로딩

--- src/main/java/com/forwork/backend/api/order/controlloer/OrderController.java
+++ src/main/java/com/forwork/backend/api/order/controlloer/OrderController.java
@@ -1,5 +1,6 @@
 package com.forwork.backend.api.order.controlloer;
 
+import com.forwork.backend.api.order.dto.request.CashReceiptIssueRequest;
 import com.forwork.backend.api.order.dto.request.OrderRequestDTO;
 import com.forwork.backend.api.order.dto.response.OrderResponseDTO;
 import com.forwork.backend.api.order.service.OrderService;
@@ -46,6 +47,26 @@ public class OrderController {
         OrderResponseDTO response = orderService.createOrder(securityMember.getId(), orderRequestDTO);
 
         return ApiResponse.success(SuccessStatus.ORDER_CREATE_SUCCESS, response);
+    }
+
+    @Operation(summary = "현금영수증 발행  (용범)", description =
+            "입력: CashReceiptIssueRequest" +
+                    "<p>" +
+                    "참고: <a href=\"https://docs.tosspayments.com/reference#현금영수증-발급-요청\" target=\"_blank\">현금영수증 발급 요청</a>" +
+                    "<p>" +
+                    "예외: <a href=\"https://docs.tosspayments.com/reference/error-codes#%ED%98%84%EA%B8%88%EC%98%81%EC%88%98%EC%A6%9D-%EB%B0%9C%EA%B8%89-%EC%9A%94%EC%B2%AD\" target=\"_blank\">이동하기</a>"
+    )
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "현금영수증 발급 성공"),
+    })
+    @PostMapping("/{merchant-order-id}/cash-receipt")
+    public ResponseEntity<ApiResponse<Void>> issueCashReceipt(@Valid @RequestBody CashReceiptIssueRequest request,
+                                                              @AuthenticationPrincipal SecurityMember securityMember,
+                                                              @PathVariable("merchant-order-id") String merchantOrderId) {
+
+        orderService.issueCashReceipt(securityMember.getId(), request, merchantOrderId);
+
+        return ApiResponse.success(SuccessStatus.CASH_RECEIPT_ISSUE_SUCCESS);
     }
 
 
--- src/main/java/com/forwork/backend/common/response/SuccessStatus.java
+++ src/main/java/com/forwork/backend/common/response/SuccessStatus.java
@@ -151,6 +151,8 @@ public enum SuccessStatus {
     CREATE_SPEC_SUCCESS(HttpStatus.CREATED, "스펙 등록 성공"),
     SPEC_EVALUATION_SUCCESS(HttpStatus.CREATED, "스펙 평가 완료"),
     ORDER_GET_SUCCESS(HttpStatus.OK, "주문 조회 성공"),
+    CASH_RECEIPT_ISSUE_SUCCESS(HttpStatus.CREATED, "현금영수증 발급 성공"),
+
 
 
     ;
