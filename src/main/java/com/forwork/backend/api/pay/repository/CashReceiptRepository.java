393707b85a76e6106f9c788a4683d269bde03f1b
22DT <qab3312@gmail.com>
2026-03-24 16:55:07 +0900

    [FEAT] 현금영수증 조회 쿼리 추가

--- src/main/java/com/forwork/backend/api/pay/repository/CashReceiptRepository.java
+++ src/main/java/com/forwork/backend/api/pay/repository/CashReceiptRepository.java
@@ -2,7 +2,14 @@ package com.forwork.backend.api.pay.repository;
 
 import com.forwork.backend.api.pay.entity.CashReceipt;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface CashReceiptRepository extends JpaRepository<CashReceipt, Long> {
+    @Query("select cr from CashReceipt cr " +
+            "where cr.merchantOrderId = :merchantOrderId")
+    Optional<CashReceipt> findByMerchantOrderId(@Param("merchantOrderId") String merchantOrderId);
 
 }
