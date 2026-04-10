6c5cf91be11f8fe6ec3d570bb56f84e672912ff4
22DT <qab3312@gmail.com>
2026-04-10 18:23:01 +0900

    [FEAT] 결제 내역 전략 추가

---
+++ src/main/java/com/forwork/backend/api/pay/service/PaymentHistoryStrategy.java
@@ -0,0 +1,13 @@
+package com.forwork.backend.api.pay.service;
+
+import com.forwork.backend.api.order.enums.ItemType;
+import com.forwork.backend.api.pay.dto.response.PaymentHistoryResponse;
+import com.forwork.backend.api.pay.entity.Payment;
+
+import java.util.List;
+
+public interface PaymentHistoryStrategy {
+    boolean supports(ItemType type);
+
+    List<PaymentHistoryResponse> map(List<Payment> payments);
+}
