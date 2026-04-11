030d582fe06703141571a7a2b2d4d16a86bd2925
22DT <qab3312@gmail.com>
2026-04-11 14:47:08 +0900

    [FEAT] 구독 상태 변경 쿼리 추가

--- src/main/java/com/forwork/backend/api/plan/repository/SubscriptionRepository.java
+++ src/main/java/com/forwork/backend/api/plan/repository/SubscriptionRepository.java
@@ -2,8 +2,10 @@ package com.forwork.backend.api.plan.repository;
 
 import com.forwork.backend.api.plan.entity.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -12,7 +14,14 @@ public interface SubscriptionRepository extends JpaRepository<Subscription, Long
     @Query("select s from Subscription s" +
             " join fetch s.planVersion pv" +
             " join fetch pv.plan" +
-            " where s.member.id=:memberId and s.subscriptionType='ACTIVE'")
+            " where s.member.id=:memberId and s.subscriptionStatus='ACTIVE'")
     Optional<Subscription> findActiveSubscriptionByMemberId(@Param("memberId") Long memberId);
+
+    @Modifying
+    @Transactional
+    @Query("update Subscription s" +
+            " set s.subscriptionStatus=:toStatus" +
+            " where s.member.id=:memberId and s.subscriptionStatus=:fromStatus")
+    long updateStatus(@Param("memberId") Long memberId, @Param("fromStatus") String fromStatus, @Param("toStatus") String toStatus);
 
 }
