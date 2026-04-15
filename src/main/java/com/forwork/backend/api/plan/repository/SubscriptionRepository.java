4f193339fe3a2fffd0073821085d067e763ff412
22DT <qab3312@gmail.com>
2026-04-15 14:20:14 +0900

    [FEAT] 만료된 PRO 구독 조회

--- src/main/java/com/forwork/backend/api/plan/repository/SubscriptionRepository.java
+++ src/main/java/com/forwork/backend/api/plan/repository/SubscriptionRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
@@ -17,11 +19,25 @@ public interface SubscriptionRepository extends JpaRepository<Subscription, Long
             " where s.member.id=:memberId and s.subscriptionStatus='ACTIVE'")
     Optional<Subscription> findActiveSubscriptionByMemberId(@Param("memberId") Long memberId);
 
+
+    @Query("select s from Subscription s" +
+            " join fetch s.member" +
+            " where s.subscriptionStatus='ACTIVE' and s.endDate <= :endDate")
+    List<Subscription> findExpiredActiveSubscriptions(@Param("endDate")LocalDate endDate);
+
     @Modifying
     @Transactional
     @Query("update Subscription s" +
             " set s.subscriptionStatus=:toStatus" +
             " where s.member.id=:memberId and s.subscriptionStatus=:fromStatus")
     long updateStatus(@Param("memberId") Long memberId, @Param("fromStatus") String fromStatus, @Param("toStatus") String toStatus);
+
+
+    @Modifying
+    @Transactional
+    @Query("update Subscription s" +
+            " set s.subscriptionStatus=:toStatus" +
+            " where s.id in :subscriptionIds")
+    long updateStatus(@Param("subscriptionIds") List<Long> subscriptionIds, @Param("toStatus") String toStatus);
 
 }
--- src/main/java/com/forwork/backend/api/plan/service/SubscriptionReader.java
+++ src/main/java/com/forwork/backend/api/plan/service/SubscriptionReader.java
@@ -7,6 +7,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.util.List;
+
 import static com.forwork.backend.common.response.ErrorStatus.PLAN_NOT_FOUND_EXCEPTION;
 
 @Component
@@ -29,4 +34,19 @@ public class SubscriptionReader {
 
         return subscription;
     }
+
+
+    /**
+     * 하루 전 만료된 PRO 구독 조회
+     */
+    public List<Subscription> findSubscriptionsExpiredYesterday() {
+        OffsetDateTime now = OffsetDateTime.now(ZoneId.of("Asia/Seoul"));
+
+        LocalDate endDate = now.toLocalDate().minusDays(1);
+
+        List<Subscription> expiredActiveSubscriptions = subscriptionRepository.findExpiredActiveSubscriptions(endDate);
+
+        return expiredActiveSubscriptions;
+    }
+
 }
