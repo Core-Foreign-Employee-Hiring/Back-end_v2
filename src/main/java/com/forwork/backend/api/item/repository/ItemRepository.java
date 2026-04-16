e316e7d0e7677ec758669803e09bbaae3baafdc4
22DT <qab3312@gmail.com>
2026-04-16 14:48:32 +0900

    [FEAT] item 조회 기능 추가

--- src/main/java/com/forwork/backend/api/item/repository/ItemRepository.java
+++ src/main/java/com/forwork/backend/api/item/repository/ItemRepository.java
@@ -1,6 +1,8 @@
 package com.forwork.backend.api.item.repository;
 
 import com.forwork.backend.api.item.entity.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,4 +19,9 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
             " join oi.item i" +
             " where oi.order.merchantOrderId=:merchantOrderId")
     List<Item> findItemsByMerchantOrderId(@Param("merchantOrderId") String merchantOrderId);
+
+    @Query("select i from Item i " +
+            "where i.active = true and i.type = :type" +
+            " order by i.id desc")
+    Page<Item> findActiveItemsByType(@Param("type") String type, Pageable pageable);
 }
