7d6f909bfd14c2583f5085fbf9cc6ac95d113713
22DT <qab3312@gmail.com>
2026-04-16 14:49:37 +0900

    [FEAT] 플랜 조회 기능 추가

--- src/main/java/com/forwork/backend/api/plan/repository/PlanVersionRepository.java
+++ src/main/java/com/forwork/backend/api/plan/repository/PlanVersionRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PlanVersionRepository extends JpaRepository<PlanVersion, Long> {
@@ -15,4 +16,12 @@ public interface PlanVersionRepository extends JpaRepository<PlanVersion, Long> 
     @Query("select pv from PlanVersion pv" +
             " where pv.active=true and pv.plan.planType=:planType")
     Optional<PlanVersion> findPlanVersion(@Param("planType") String planType);
+
+
+    @Query("select pv from PlanVersion pv" +
+            " join fetch pv.plan" +
+            " join fetch pv.item i" +
+            " where i.id in :itemIds" +
+            " order by i.id desc")
+    List<PlanVersion> findByItemIdsWithPlan(@Param("itemIds") List<Long> itemIds);
 }
